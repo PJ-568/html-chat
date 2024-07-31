4edd67798e793a28ea55e0527a0c9512b162dd5e
PJ-568 <PJ568@PJ568.eu.org>
2024-07-31 09:12:23 +0800

    【优化】目录访问支持

--- server.py
+++ server.py
@@ -41,7 +41,6 @@ class ChatServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             if self.path == '/':
-                self.send_response(200)
                 self.send_file('index.html')
             elif self.path == '/index.html':
                 self.send_file('index.html')
